38b054c16487b14e934d7ad9700943c36630bf15
Brian <brian.m.wendt@gmail.com>
2019-04-17 22:09:40 -0700

    Markup generator for issue submission

--- docs/lib/index.js
+++ docs/lib/index.js
@@ -1,16 +1,26 @@
-var $input, $output, Doc;
+var $input, $output, $download, Doc;
 $(document).ready(function(){
     $input = $('#input');
     $output = $('#output');
+    $download = $('#download');
     Doc = new JSONSchemaMarkdown();
     run();
     $input.on("input change", run);
+    $output.focus(function () {
+        $(this).select();
+    });
 });
 
 function run(){
     Doc.load($input.val());
-    console.log(Doc.schema);
     $output.val(Doc.generate());
+    if(Doc.schema !== null){
+        var href = "data:text/plain;charset=utf-8," + encodeURIComponent(Doc.markdown);
+        var download = (typeof Doc.schema["$id"] === "string") ? Doc.schema["$id"] + '.md' : 'README.md';
+        $download.attr('href', href).attr('download', download).removeClass('d-none');    
+    } else {
+        $download.addClass("d-none");
+    }
     //$input.height( $input[0].scrollHeight );
     //$output.height( $output[0].scrollHeight );
 }
---
+++ docs/lib/issue.js
@@ -0,0 +1,27 @@
+var $text, $schema, $output;
+$(document).ready(function () {
+    $text = $('#text');
+    $schema = $('#schema');
+    $output = $('#output');
+    run();
+    $text.on("input change", run);
+    $schema.on("input change", run);
+    $output.focus(function () {
+        $(this).select();
+    });
+});
+
+function run() {
+    var markdown = "**Describe Your Issue**";
+    markdown += $text.val() + "\n\n";
+    markdown += "**Schema**\n";
+    var schema = $schema.val();
+    try {
+        var schemaObj = JSON.parse(schema);
+        schema = JSON.stringify(schemaObj);
+    } catch (e) {
+        markdown += "Invalid JSON!!!\n";
+    }
+    markdown += "\n```\n" + schema + "\n```";
+    $output.val(markdown);
+}

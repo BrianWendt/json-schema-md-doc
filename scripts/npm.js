6f173ad671925c262e7348d225fe4d1269640678
Brian <brian.m.wendt@gmail.com>
2021-01-26 10:17:15 -0700

    Fix for #10
    Changing the ignore path to only ignore the generated docs/documentation/ directory.  Increment version super minor for NPM

--- scripts/npm.js
+++ scripts/npm.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 var ignore = fs.readFileSync('./.gitignore');
 ignore += "\n";
 ignore += "# npm ignore\n";
-ignore += "docs/\n";
+ignore += "docs/documentation/\n";
 fs.writeFileSync('./.npmignore', ignore);
